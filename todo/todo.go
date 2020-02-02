4f3884c3db7924af77340e05aa1f9af8dc228084
Tiger Nie <nhl0819@gmail.com>
2020-02-02 11:18:08 -0600

    init

---
+++ todo/todo.go
@@ -0,0 +1,71 @@
+package todo
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+)
+
+type Item struct {
+	Text     string
+	Priority int
+	position int
+}
+
+type BiPri []Item
+
+func (i *Item) Label() string {
+	return strconv.Itoa(i.position) + ". "
+}
+
+func (i *Item) SetPriority(pri int) {
+	switch pri {
+	case 1:
+		i.Priority = 1
+	case 3:
+		i.Priority = 3
+	default:
+		i.Priority = 2
+	}
+}
+
+func (i *Item) PrettyP() string {
+	if i.Priority == 1 {
+		return "(1)"
+	}
+	if i.Priority == 3 {
+		return "(3)"
+	}
+	return ""
+}
+
+func SaveItems(filename string, items []Item) error {
+	b, err := json.Marshal(items)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(b))
+	err = ioutil.WriteFile(filename, b, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ReadItems(filename string) ([]Item, error) {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return []Item{}, nil
+	}
+	var items []Item
+	if err := json.Unmarshal(b, &items); err != nil {
+		return []Item{}, nil
+	}
+
+	for i, _ := range items {
+		items[i].position = i + 1
+	}
+	return items, nil
+}
